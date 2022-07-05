cf8423b618d0bc586bdbb438a66f64b7dd61c80d
dlrandy <qq1208484996@qq.com>
2022-07-05 22:33:35 +0800

    feat: fundamentals done

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Platform, StatusBar,Text, View, Image, SafeAreaView, Alert,TouchableWithoutFeedback, TouchableOpacity, TouchableNativeFeedback, Button } from "react-native";
+
 
 export default function App() {
+  const handlePress = () => console.log("text clicked");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={[styles.container, containerStyle]}>
+      <Button color={'orange'} title="Click me"
+      //  onPress={()=>alert('button clicked')}/>
+       onPress={()=>Alert.alert('title','button clicked',[
+        {text: 'yes', onPress:()=>console.log('yes')},
+        {text: 'No', onPress:()=>console.log('yes')},
+       ])}
+       />
+      <Text numberOfLines={1} onPress={handlePress}>
+        Hello World! This is a very long long long text.I wanna show you!{" "}
+      </Text>
+      {/* <Image source={require("./assets/icon.png")} /> */}
+      <TouchableOpacity onPress={()=>console.log('touchable')}>
+
+      <Image
+       blurRadius={10}
+       fadeDuration={1000}
+        source={{
+          uri: "https://picsum.photos/200/300",
+          width: 200,
+          height: 300,
+        }}
+      />
+      </TouchableOpacity>
+      <TouchableNativeFeedback onPress={()=>console.log('native touchable')}>
+        <View style={{width: 34,height:56,backgroundColor: 'gray'}}></View>
+      </TouchableNativeFeedback>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
+const containerStyle = {backgroundColor: 'green'}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "purple",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    // justifyContent: "center",
+    // alignItems: "center",
   },
 });
